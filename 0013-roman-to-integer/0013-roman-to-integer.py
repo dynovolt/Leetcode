a810e857917b4fe1bebdde5e2798b3b0d7067593
dynovolt <sankalpsridhar9@gmail.com>
2026-07-16 01:39:15 +0530

    Time: 15 ms (5.78%), Space: 19.3 MB (57.45%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,25 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        s = s.replace("IV", "IIII")
-        s = s.replace("IX", "VIIII")
-        s = s.replace("XL", "XXXX")
-        s = s.replace("XC", "LXXXX")
-        s = s.replace("CD", "CCCC")
-        s = s.replace("CM", "DCCCC")
-        
-
         roman_map = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000
         }
         
         total = 0
-        for char in s:
-            total += roman_map[char]
+        max_value = 0
+        
+        for char in reversed(s):
+            value = roman_map[char]
             
+            if value < max_value:
+                total -= value
+            else:
+             
+                total += value
+                max_value = value
+                
         return total
