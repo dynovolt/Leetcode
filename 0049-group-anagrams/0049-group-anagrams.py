fb2a6710b1d877b74f292b22b11a588f938ee7a0
dynovolt <sankalpsridhar9@gmail.com>
2026-08-03 23:51:34 +0530

    Time: 11 ms (82.56%), Space: 22.1 MB (52.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,26 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
-        visited = [False] * len(strs)
+        groups = defaultdict(list)
 
-        for i in range(len(strs)):
-            if visited[i]:
-                continue
+        for word in strs:
+            key = "".join(sorted(word))
+            groups[key].append(word)
 
-            group = [strs[i]]
-            visited[i] = True
-
-            for j in range(i + 1, len(strs)):
-                if not visited[j] and self.isAnagram(strs[i], strs[j]):
-                    group.append(strs[j])
-                    visited[j] = True
-
-            result.append(group)
-
-        return result
-
-    def isAnagram(self, s1: str, s2: str) -> bool:
-        if len(s1) != len(s2):
-            return False
-
-        return sorted(s1) == sorted(s2)
+        return list(groups.values())
