286859f3470571e1cc651c32328a9fa7d8bb7766
dynovolt <sankalpsridhar9@gmail.com>
2026-08-07 22:47:25 +0530

    Time: 8 ms (49.23%), Space: 23.6 MB (5.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = ""
+        cleaned = []
         for ch in s:
             if ch.isalnum():
-                cleaned += ch.lower()
-        reversed_string = cleaned[::-1]
-        return cleaned == reversed_string
+                cleaned.append(ch.lower())
+        cleaned = "".join(cleaned)
+        return cleaned == cleaned[::-1]
