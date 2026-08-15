978a7d0bef582baf971e9f3181b84172bec7e272
dynovolt <sankalpsridhar9@gmail.com>
2026-08-15 23:15:13 +0530

    Time: 8 ms (22.58%), Space: 20.5 MB (62.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,7 +1,9 @@
 class Solution:
     def moveZeroes(self, nums):
         j = 0
+
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
+                if i != j:
+                    nums[i], nums[j] = nums[j], nums[i]
                 j += 1
